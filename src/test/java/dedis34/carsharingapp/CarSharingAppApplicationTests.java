674c6b3d3510078a58fb1bbc33b4ca8ca59f3654
Artur <a_blyskal@wp.pl>
2025-06-08 13:01:14 +0200

    Initial commit

---
+++ src/test/java/dedis34/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dedis34.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
